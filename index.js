a1e6fd0a87596d7fd4638060a5827a537e1e382d
Pushpendra Singh <99976461+govind124@users.noreply.github.com>
2022-09-21 14:24:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const clock=()=>{
     let time= new Date();
-    let amorpm="";
+    // let amorpm="AM"
     let hour=time.getHours();
     let minute=time.getMinutes();
     let second=time.getSeconds();
     let hours=document.getElementsByClassName("hour");
     let minutes=document.getElementsByClassName("minute");
-    let seconds=document.getElementsByClassName("seconds");
+    let seconds=document.getElementsByClassName("second");
     let am_pm=document.getElementsByClassName("am_pm");
     
-    if(hour>12){
-        hour=hour-12;
-        amorpm="PM";
-    }
-    if(hour==0){
-        hour=12;
-        amorpm="AM";
-    }
-    if(hour<10){
-        hour="0"+hour;
-    }
-    if(minute<10){
-        minute="0"+minute;
-    }
-    if(second<10){
-        second="0"+second;
-    }
-    hours[0].innerText=hour;
-    minutes[0].innerText=minute;
-    seconds[0].innerText=second;
-    am_pm[0].innerText=amorpm;
-    }
+  if(hour>12){
+    hour=hour-12;
+}
+if(hour<10){
+    hour="0"+hour;
+}
+if(minute<10){
+    minute="0"+minute;
+}
+if(second<10){
+    second="0"+second;
+}
+if(hour=>12){
+    console.log('----12----am/pm----'+am_pm[0]);
+    am_pm[0].innerText='AM'
+}
+if(hour==0){
+    hour=12;
+}
+hours[0].innerText=hour;
+minutes[0].innerText=minute;
+seconds[0].innerText=second;
+// am_pm[0].innerText=amorpm;
+}
     
         const setAlarm=()=>{
         let timing=document.getElementsByClassName("timing");
